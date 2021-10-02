0d66fec3315546300a67ecaee9ff0564ad2fd30d
jendevane <87095302+jendevane@users.noreply.github.com>
2021-10-01 22:10:07 -0400

    added cats

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,77 @@
-
-var options = {
-    "apikey": "ntjbOl80",
-    "objectType": "animals",
-    "objectAction": "publicSearch",
-    "search" : {
-        "calcFoundRows": "Yes",
-        "resultStart": 0,
-        "resultLimit": 20,
-        "resultSort": "animalID",
-        "fields": [
-            "animalID",
-            "animalOrgID",
-            "animalName",
-            "animalSpecies",
-            "animalBreed",
-            "animalThumbnailUrl",
-            "animalLocation"
-        ],
-        "filters": [
-            {
-                "fieldName": "animalStatus",
-                "operation": "equals",
-                "criteria": "Available"
-            }, 
-            {
-                "fieldName": "animalLocationDistance",
-                "operation": "radius",
-                "criteria": "50"
-            }, 
-            {
-                "fieldName": "animalLocation",
-                "operation": "equals",
-                "criteria": "77056"
-            },
-        ]
-    }
-};
-var res = fetch('https://api.rescuegroups.org/http/v2.json', { 
-    method: 'post',
-    body: JSON.stringify(options)
-    
-    }).then(function(response) {
-        debugger;
+function getCats() {
+    // get value of zipcode field
+    let zip = document.getElementById('zipCode')
+    let zipCode = zip.value
+    var options = {
+        "apikey": "ntjbOl80",
+        "objectType": "animals",
+        "objectAction": "publicSearch",
+        "search": {
+            "calcFoundRows": "Yes",
+            "resultStart": 0,
+            "resultLimit": 50,
+            "fields": [
+                "animalID",
+                "animalOrgID",
+                "animalName",
+                "animalSpecies",
+                "animalBreed",
+                "animalThumbnailUrl",
+                "animalLocation"
+            ],
+            "filters": [
+                {
+                    "fieldName": "animalStatus",
+                    "operation": "equals",
+                    "criteria": "Available"
+                },
+                {
+                    "fieldName": "animalLocationDistance",
+                    "operation": "radius",
+                    "criteria": "50"
+                },
+                {
+                    "fieldName": "animalLocation",
+                    "operation": "equals",
+                    "criteria": zipCode // use user zipcode input in search
+                },
+            ]
+        }
+    };
+    fetch('https://api.rescuegroups.org/http/json', {
+        method: 'post',
+        dataType: "jsonp",
+        body: JSON.stringify(options)
+    }).then(function (response) {
+        // make results readable
         return response.json();
-    }).then(function(data) {
-        console.log(data);
-        debugger;
+    }).then(function (response) {
+        // set cats to the data field returned
+        let cats = response.data
+        // make new empty array
+        let catsArray = []
+        for (const value in cats) {
+            // push in the cat objects
+            catsArray.push(cats[value])
+        }
+        console.log(catsArray);
+        // loop through the cat array and add the field values to the page
+        catsArray.forEach((cat) => {
+            var responseContainerEl = document.getElementById('cats-container');
+            var printCat = document.createElement('div');
+            // make image element and set it to the url from the data
+            var catImage = document.createElement('img');
+            catImage.setAttribute('src', cat.animalThumbnailUrl)
+            // add all the values you want here
+            printCat.innerHTML += '<strong>Name: </strong>' + cat.animalName
+            printCat.innerHTML += '<strong>Breeed: </strong>' + cat.animalBreed
+            // append the image
+            printCat.appendChild(catImage)
+            // print to page
+            responseContainerEl.appendChild(printCat);
+        })
     });
+}
 
     
    
